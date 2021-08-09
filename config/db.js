fa80b323c1f4acf4053b4143e32d62e3c84bb2ca
Guido Varela Coletta <guido.varela@gmail.com>
2021-08-09 02:47:34 -0300

    upload

--- config/db.js
+++ config/db.js
@@ -1,16 +1,21 @@
 //Conexion MySQL
 const Sequelize = require('sequelize')
 
-//conexion local -> wamp
+/*conexion local -> wamp
 let database = 'utn2021'
 let userMYSQL = 'root'
 let passMySQL = ''
-let hostMySQL = '127.0.0.1'
+let hostMySQL = '127.0.0.1'*/
 
+const remote = {
+  user: "bd9a8ea7d59e96",
+  pass: "3c237954",
+  host: "us-cdbr-east-04.cleardb.com",
+  nameDB:"heroku_e52358cd71fcfab"
+}
 
-
-const db = new Sequelize(database, userMYSQL, passMySQL, {
-  host: hostMySQL,
+const db = new Sequelize(remote.nameDB, remote.user, remote.pass, {
+  host: remote.host,
   dialect: 'mysql',
   define: {
         //desactivamos el timestamp, para que no envie fechas de actualizaciones a la db
